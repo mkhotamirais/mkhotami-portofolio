9e30ce68d64b6acb80101acabdfc76cb37f876b5
Mkhotamirais <mkhotamirais@gmail.com>
2024-04-29 09:07:04 +0700

    portofolio update

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,7 +5,7 @@ const projectsList = [
   {
     name: "todo list 1",
     description:
-      "This project provides CRUD operation features with simple notification. The data is stored in localStorage. The react hook used is only useState and useEffect.",
+      "This project offers CRUD operation functionalities with straightforward notification mechanisms. Data is stored in the localStorage, and the implementation relies solely on React hooks, specifically useState and useEffect.",
     tools: "Front-end: React, Redux, Tailwindcss, ReactIcons. Back-end: localStorage",
     githubLink: "https://github.com/mkhotamirais/panduan-react/tree/main/src/pages/mini-projects/react-crud",
     demoLink: "https://panduan-reactjs.vercel.app/mini-projects/react-crud",
@@ -13,7 +13,7 @@ const projectsList = [
   {
     name: "todo list 2",
     description:
-      "This project provides CRUD operation features with notification by Notistack. The data is stored in localStorage. The react hook used is only useState, useEffect, useReducer dan useContext.",
+      "This project offers CRUD operation functionalities with notification capability facilitated by Notistack. Data is stored in localStorage, and the implementation utilizes React hooks including useState, useEffect, useReducer, and useContext.",
     tools: "Front-end: React, Redux, Tailwindcss, ReactIcons, Notistack. Back-end: localStorage",
     githubLink: "https://github.com/mkhotamirais/panduan-react/tree/main/src/pages/mini-projects/todo-list",
     demoLink: "https://panduan-reactjs.vercel.app/mini-projects/todo-list",
@@ -21,7 +21,7 @@ const projectsList = [
   {
     name: "sisko",
     description:
-      "This project displays products data from public api sistem toko in grid system cards form. Includes viewport for any device. Equipped with dark mode, breadcrumb, data filter and data search.",
+      "Developed a project that retrieves and displays product data from a public API, SistemToko, utilizing grid system cards for efficient presentation. Implemented a responsive design approach, ensuring seamless viewport adaptation across various devices. Incorporated advanced features such as dark mode, breadcrumb navigation, data filtering, and search functionality to enhance user experience and accessibility.",
     tools: "Front-end: React, Redux, Axios, Reacticons, Tailwindcss. Back-end: public api sistem toko",
     githubLink: "https://github.com/mkhotamirais/panduan-public-apis/tree/main/src/pages/apis/sisko",
     demoLink: "https://panduan-public-apis.vercel.app/sisko/product",
@@ -29,8 +29,7 @@ const projectsList = [
   {
     name: "jsonplaceholder",
     description:
-      "This project displays posts and users data from public api Jsonplaceholder in grid system cards form. Includes viewport for any device. Equipped with dark mode, breadcrumb and dummy crud.",
-
+      "This project showcases posts and user data retrieved from the public API, Jsonplaceholder. The data is presented in a grid system card format, ensuring compatibility across various viewport sizes. It features dark mode functionality, breadcrumbs, and dummy CRUD operations.",
     tools: "Front-end: React, Redux, Axios, Reacticons, Tailwindcss. Back-end: public api Jsonplaceholder",
     githubLink: "https://github.com/mkhotamirais/panduan-public-apis/tree/main/src/pages/apis/jsonplaceholder/post",
     demoLink: "https://panduan-public-apis.vercel.app/jsonplaceholder/posts",
@@ -38,8 +37,7 @@ const projectsList = [
   {
     name: "omdbapi",
     description:
-      "This project displays movies data from public api Omdbapi in grid system cards form. Includes viewport for any device. Equipped with dark mode, breadcrumb, data filter and data search.",
-
+      "This project showcases movie data retrieved from the public API, Omdbapi. The data is presented in a grid system card format, ensuring compatibility across various viewport sizes. It includes dark mode, breadcrumbs, data filtering, and search functionality.",
     tools: "Front-end: React, Redux, Axios, Reacticons, Tailwindcss. Back-end: public api Omdbapi",
     githubLink: "https://github.com/mkhotamirais/panduan-public-apis/tree/main/src/pages/apis/omdbapi/movie",
     demoLink: "https://panduan-public-apis.vercel.app/omdbapi/movies",
@@ -47,7 +45,7 @@ const projectsList = [
   {
     name: "kamus mini",
     description:
-      "This project provides simple live search with limited result. The data is an array of objects containing a list of terms and their meanings, stored manually in javascript file.",
+      "This project provides a simple live search with limited results. The data is an array of objects containing a list of terms and their meanings, stored manually in a javascript file.",
     tools: "Front-end: React, Redux, Tailwindcss, ReactIcons",
     githubLink: "https://github.com/mkhotamirais/mkhotami-ringkasan",
     demoLink: "https://mkhotami-ringkasan.vercel.app/",
