e202ef42c25fc1e449381ee93dd43854fcc46b18
Mkhotamirais <mkhotamirais@gmail.com>
2024-06-05 10:43:29 +0700

    update project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,7 +5,7 @@ const projectsList = [
   {
     name: "todo",
     description:
-      "This project explores 3 todo list implementations with identical CRUD functionality. Each uses different tools: some leverage React hooks useState, useEffect, useContext, and useReducer, while another employs Redux. Notifications vary: some are basic, while others utilize Notistack or React Hot Toast libraries. All data is stored in local storage.",
+      "This project explores 3 todo list implementations with identical CRUD functionality. Each uses different tools: some leverage React hooks useState, useEffect, useContext, and useReducer, while another employs Redux. Notifications vary: some are basic, while others utilize Notistack or React Hot Toast libraries. All data is stored in local storage. The development progress of this project has reached 96%. Despite appearing complete, there is a possibility that additional features will be added in the future.",
     tools: "Front-end: React, Redux, Tailwindcss, React-Icons, notistack, react-hot-toast; Back-end: localStorage",
     githubLink: "https://github.com/mkhotamirais/mkhotami/tree/main/src/pages/client-app/todo",
     demoLink: "https://mkhotami.vercel.app/client-app/todo",
@@ -13,7 +13,7 @@ const projectsList = [
   {
     name: "mkhotami app",
     description:
-      "This project is a collection of simple MERN stack projects that encompass CRUD HTTP requests, authentication, and authorization features. Additionally, it includes data search, pagination, filtering, and sorting functionalities. One of the projects in this collection is a dictionary application.",
+      "This project is a collection of simple MERN stack projects that encompass CRUD HTTP requests, authentication, and authorization features. Additionally, it includes data search, pagination, filtering, and sorting functionalities. One of the projects in this collection is a dictionary application. The development of this project is only at 60% completion, as several other mini projects, including the Note project, will be added soon.",
     tools:
       "Front-end: React, Redux, React-router-dom, Tailwindcss, React-Icons, React-hot-toast; Backend: Nodejs, Expressjs, Mongoose. Bcrypt, Jwt, etc",
     githubLink: "https://github.com/mkhotamirais/mkhotami/tree/main/src/pages/mern-app",
@@ -22,16 +22,16 @@ const projectsList = [
   {
     name: "mkhotami warung",
     description:
-      "This project displays product data in a table and grid system layout. The data is retrieved from a MongoDB database and managed on the server side using Express.js. It includes authentication and authorization features where only admins can manage the data, while guests and users have restricted access. The project also offers search, filtering, and sorting functionalities, as well as pagination. Additionally, it is responsive to different viewports and includes a dark mode feature.",
+      "This project displays product data in a table and grid system layout. The data is retrieved from a MongoDB database and managed on the server side using Express.js. It includes authentication and authorization features where only admins can manage the data, while guests and users have restricted access. The project also offers search, filtering, and sorting functionalities, as well as pagination. Additionally, it is responsive to different viewports and includes a dark mode feature. The development of this project is approximately 80% complete, as the cart and invoice features have not yet been implemented.",
     tools:
-      "Front-end: React, Redux, React-router-dom, ReactIcons, Tailwindcss, React-hot-toast. Backend: Nodejs, Expressjs, Mongoose. Bcrypt, Jwt, etc.",
+      "Front-end: React, Redux, ReactIcons, Tailwindcss, React-hot-toast. Backend: Nodejs, Expressjs, Mongoose. Bcrypt, Jwt, etc.",
     githubLink: "https://github.com/mkhotamirais/warung-ota",
     demoLink: "https://warung-ota.vercel.app/",
   },
   {
     name: "public api",
     description:
-      "This project showcases data retrieved from public APIs, including Jsonplaceholder, OmdbAPI, FakestoreAPI, SistemToko, and NewsAPI. The data is presented in a grid system card format, ensuring compatibility across various viewport sizes. Features include dark mode, breadcrumbs, data filtering, and search functionality to enhance user experience and accessibility.",
+      "This project showcases data retrieved from public APIs, including Jsonplaceholder, OmdbAPI, FakestoreAPI, SistemToko, and NewsAPI. The data is presented in a grid system card format, ensuring compatibility across various viewport sizes. Features include dark mode, breadcrumbs, data filtering, and search functionality to enhance user experience and accessibility. The development of this project is approximately 70% complete, as additional another public APIs will be integrated.",
     tools:
       "Front-end: React, Redux, Axios, Reacticons, Tailwindcss; Back-end: Public APIs (Jsonplaceholder, OmdbAPI, FakestoreAPI, SistemToko, and NewsAPI)",
     githubLink: "https://github.com/mkhotamirais/mkhotami/tree/main/src/pages/client-app",
@@ -40,7 +40,7 @@ const projectsList = [
 ];
 
 const Projects = () => {
-  const [active, setActive] = useState("todo list 1");
+  const [active, setActive] = useState("todo");
   return (
     <div>
       <div className="flex flex-wrap justify-center gap-3 mb-5">
